825c0dfb744ccec1533424d6e1a5096013706dce
Miguel Felipe Peñuela Garzón <36526084+felip2018@users.noreply.github.com>
2026-02-19 17:27:00 -0500

    add shared components

--- src/app/layout/website/website.ts
+++ src/app/layout/website/website.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {NavigationMenu} from '../../shared/components/navigation-menu/navigation-menu';
+import {ContactInfo} from '../../shared/components/contact-info/contact-info';
 
 @Component({
   selector: 'app-website',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NavigationMenu,
+    ContactInfo
   ],
   templateUrl: './website.html',
   styleUrl: './website.scss',
