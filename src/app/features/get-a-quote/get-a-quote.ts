3beda1c395dacbd997fc27aba08047e4a4ac134f
felip2018 <felipegarxon@hotmail.com>
2026-02-26 19:26:55 -0500

    update

---
+++ src/app/features/get-a-quote/get-a-quote.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-a-quote',
+  imports: [],
+  templateUrl: './get-a-quote.html',
+  styleUrl: './get-a-quote.scss',
+  standalone: true
+})
+export class GetAQuote {
+
+}
--- src/app/shared/components/navigation-menu/navigation-menu.ts
+++ src/app/shared/components/navigation-menu/navigation-menu.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {ContactInfo} from "../contact-info/contact-info";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-navigation-menu',
-  imports: [],
+  imports: [
+    ContactInfo,
+    RouterLink
+  ],
   templateUrl: './navigation-menu.html',
   styleUrl: './navigation-menu.scss',
   standalone: true
